31b17e711b42a63a177f8d694612c1a96332cdea
applesline <applesline@163.com>
2023-12-15 21:33:41 +0800

    Add files via upload

---
+++ WalletStatus.tsx
@@ -0,0 +1,37 @@
+import { useCurrentAccount } from "@mysten/dapp-kit";
+import { Container, Flex, Heading, Text } from "@radix-ui/themes";
+import { OwnedObjects } from "./OwnedObjects";
+import {Game} from "./Game";
+import './Game.css'; 
+
+export function WalletStatus() {
+  const account = useCurrentAccount();
+
+  return (
+    <Container my="2">
+      {/* { <Heading mb="2">Wallet Status</Heading>
+
+      {account ? (
+        <Flex direction="column">
+          <Text>Wallet connected</Text>
+          <Text>Address: {account.address}</Text>
+        </Flex>
+      ) : (
+        <Text>Wallet not connected</Text>
+      )} } */
+      
+        account ? (
+          <Flex direction="column">
+          <Game/>
+          </Flex>
+        ) : (
+          <Flex direction="column">
+            <Text className="text-center">Wallet not connected</Text>
+          </Flex>
+          
+        )
+      }
+      
+    </Container>
+  );
+}
