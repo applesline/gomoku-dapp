a2b79624f7e0dc48d0d1cebbfdf559819e092f6d
applesline <applesline@163.com>
2023-12-15 21:38:23 +0800

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { ConnectButton } from "@mysten/dapp-kit";
+import { Box, Container, Flex, Heading } from "@radix-ui/themes";
+import { WalletStatus } from "./WalletStatus";
+import { createNetworkConfig,SuiClientProvider } from "@mysten/dapp-kit";
+import { getFullnodeUrl } from '@mysten/sui.js/client';
+
+
+const { networkConfig}= createNetworkConfig({
+	testnet: {url: getFullnodeUrl("testnet")},
+	localnet: { url: getFullnodeUrl("localnet")}
+});
+
+function App() {
+  return (
+    <SuiClientProvider networks={networkConfig} network="testnet">
+      <Flex
+        position="sticky"
+        px="4"
+        py="2"
+        justify="between"
+        style={{
+          borderBottom: "1px solid var(--gray-a2)",
+        }}
+      >
+        <Box>
+          <Heading></Heading>
+        </Box>
+
+        <Box>
+          <ConnectButton />
+        </Box>
+      </Flex>
+      <Container>
+        {/* <Container
+          mt="5"
+          pt="2"
+          px="4"
+          style={{ background: "var(--gray-a2)", minHeight: 500 }}
+        >
+          
+        </Container> */}
+        <WalletStatus />
+      </Container>
+    	</SuiClientProvider>
+  );
+}
+
+export default App;
